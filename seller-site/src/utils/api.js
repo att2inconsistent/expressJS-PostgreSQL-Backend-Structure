351d28b75771e2e33123aa9025bccc69f35adbec
Att <m.attayaardhana02@gmail.com>
2026-09-10 09:01:48 +0700

    Frontend-seller Stg 4b: Fixed baseURL

--- seller-site/src/utils/api.js
+++ seller-site/src/utils/api.js
@@ -1,5 +1,5 @@
 import axios from 'axios';
-const api = axios.create({ baseURL: "-" });
+const api = axios.create({ baseURL: import.meta.env.VITE_API_BASE_URL });
 
 api.interceptors.request.use((config)=> {
         const accessToken = localStorage.getItem("token");
