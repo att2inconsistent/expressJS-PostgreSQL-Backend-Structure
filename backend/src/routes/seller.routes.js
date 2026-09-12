f7d220902e0dbde5fe2ad1e499fdea5018896c7e
Att <m.attayaardhana02@gmail.com>
2026-09-13 04:18:50 +0700

    Stg 14b: Added self assignment checker route (i forgot)

--- backend/src/routes/seller.routes.js
+++ backend/src/routes/seller.routes.js
@@ -1,9 +1,10 @@
 const express = require('express');
 const router = express.Router();
-const {toggleStandOpenCotroller, getMyApplicationController} = require('../controllers/seller.controller')
+const {toggleStandOpenCotroller, getMyApplicationController, getMyAssignmentController} = require('../controllers/seller.controller')
 const authenticate = require('../middleware/authenticate')
 
 router.patch('/stands/:id/toggle-open', authenticate, toggleStandOpenCotroller)
 router.get('/my-application', authenticate, getMyApplicationController)
+router.get('/my-assignment', authenticate, getMyAssignmentController)
 
 module.exports=router
